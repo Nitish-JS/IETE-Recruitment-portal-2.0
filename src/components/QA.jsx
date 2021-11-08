b2325096253ec6e779579137b0a5117a1467ac98
Vidipt Khetriwal <mohangoyal@Vidipts-MacBook-Pro.local>
2021-11-08 15:30:30 +0530

    init-commit

---
+++ src/components/QA.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QA extends Component {
+    render() {
+        return (
+            <div>
+                <h1>QA Page</h1>
+            </div>
+        )
+    }
+}
