76c3071e97d2da24203b3542e60de6b017b20853
Nitish-JS <jsnitish46@gmail.com>
2021-11-22 19:06:19 +0530

    addd

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Home from "./Home";
-import Info from "./Info";
-import QA from "./Info";
+// import Info from "./Info";
+// import QA from "./Info";
 import Todo from "./Todo";
 import Wait from "./Wait";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -12,17 +12,17 @@ const Routing = () => {
     return (
         <div>
             <Router>
-                <Navbar />
+                {/* <Navbar /> */}
                 <Switch>
                     <Route path="/home" exact>
                     <Home />
                     </Route>
-                    <Route path="/info">
+                    {/* <Route path="/info">
                     <Info />
-                    </Route>
-                    <Route path="/qa" exact>
+                    </Route> */}
+                    {/* <Route path="/qa" exact>
                     <QA />
-                    </Route>
+                    </Route> */}
                     <Route path="/todo">
                     <Todo />
                     </Route>
