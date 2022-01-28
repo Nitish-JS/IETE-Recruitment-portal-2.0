31747dea811ac0719fdda911ff0e8fc456ed176b
Vidipt Khetriwal <mohangoyal@Vidipts-MacBook-Pro.local>
2022-01-29 00:05:43 +0530

    final commit

--- src/components/Quiz/Clock.jsx
+++ src/components/Quiz/Clock.jsx
@@ -12,71 +12,23 @@ try {
 }
 
 const Clock = (props) => {
-  // let sec = (props.minutes) * 60;
-  const [seconds, setSeconds] = useState(props.minutes * 60);
-  console.log("Props:", props.minutes);
-  console.log("Seconds after Props:", seconds);
-  // const [random, setRandom] = useState(false);
-
-  let time_left_mins;
-
-  // useEffect(async () => {
-  //   await fetch("https://recportal-iete.herokuapp.com/auth/q/", {
-  //     method: "POST",
-  //     headers: { Authorization: token, "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       domain: localStorage.getItem("domain"),
-  //     }),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       console.log("RESPONSE: ", json);
-
-  //       let totalDuration = json.totalduration;
-  //       let startTime = json.starttime;
-  //       var startTime_moment = moment(startTime, "HH:mm:ss a");
-  //       var endTime = startTime_moment.add(totalDuration, "minutes");
-  //       console.log("START TIME:", startTime);
-
-  //       var today = new Date();
-  //       var time = today.toLocaleTimeString();
-  //       var currentTime_moment = moment(time, "HH:mm:ss a");
-  //       console.log("CURRENT TIME:", time);
-
-  //       var time_left = moment.duration(endTime.diff(currentTime_moment));
-  //       time_left_mins = time_left._data.minutes;
-  //       console.log("TIME LEFT FINAL: ", time_left_mins + 1);
-  //       setSeconds((time_left._data.minutes + 1) * 60);
-  //       console.log("Seconds in 1st use effect:", seconds);
-
-  //       // setSeconds(json.totalduration * 60);
-  //     });
-  // }, []);
-
-  // React.useEffect(() => {
-  //   if (time_left_mins) {
-  //     console.log("TIME LEFT MINS:", time_left_mins);
-  //     if (seconds > 0) {
-  //       console.log("Inside IF ELSE", seconds);
-  //       setTimeout(() => setSeconds(seconds - 1), 1000);
-  //       console.log(seconds);
-  //     } else if (seconds == 0) {
-  //       window.location.href = "/endquiz";
-  //       // props.handleClock();
-  //     }
-  //   }
-  // });
+  let sec = Number(props.minutes) * 60;
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    setSeconds(sec);
+  }, [sec, props.minutes]);
 
   React.useEffect(() => {
-    console.log("SECONDS in Clock: ", seconds);
-    console.log(typeof seconds);
-    if (seconds > 0) {
-      console.log(seconds);
-      setTimeout(() => setSeconds(seconds - 1), 1000);
-      console.log(seconds);
-    } else if (seconds == 0) {
-      window.location.href = "/thankyou";
-      props.handleClock();
+    if (seconds !== 0) {
+      console.log("SECONDS in Clock: ", seconds);
+      if (seconds > 1) {
+        sec = sec - 1;
+        setTimeout(() => setSeconds(seconds - 1), 1000);
+      }
+      if (seconds == 1) {
+        window.location.href = "/thankyou";
+        props.handleClock();
+      }
     }
   });
 
@@ -94,7 +46,6 @@ const Clock = (props) => {
       >
         {Math.floor(seconds / 60)} : {seconds % 60}
       </h1>
-      {/* <h1 style={{ color: "white" }}>Clock</h1> */}
     </div>
   );
 };
